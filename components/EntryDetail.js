674e6cac7725a4eede8ff71afe8760a3f9f693d8
sebagruia <sebastiangruia@gmail.com>
2020-06-17 11:03:38 +0200

    Added EntryDetail component

---
+++ components/EntryDetail.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+class EntryDetail extends Component {
+
+  setTitle = (entryId) => {
+    if (!entryId) {
+      return;
+    }
+    const year = entryId.slice(0, 4);
+    const month = entryId.slice(5, 7);
+    const day = entryId.slice(8);
+    this.props.navigation.setOptions({ title: `${day}/${month}/${year}` });
+  };
+
+  render() {
+    const { entryId } = this.props.route.params;
+    this.setTitle(entryId);
+    return (
+      <View>
+        <Text>EntryDetail: {entryId}</Text>
+      </View>
+    );
+  }
+}
+
+export default EntryDetail;
